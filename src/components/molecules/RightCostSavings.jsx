e8f22e6f59c6883e40e42bb17fe0ad2296ef31ec
gauthamhc <gautham.hc@gmail.com>
2021-07-07 11:05:23 +0530

    costing page ready

--- src/components/molecules/RightCostSavings.jsx
+++ src/components/molecules/RightCostSavings.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { connect } from 'react-redux';
 import { Button } from './../atoms/Button.jsx';
+import { Text } from './../atoms/Text';
+import { device } from '../../theme';
 const connectedProps = (state) => ({
   user: state,
 });
@@ -15,34 +17,112 @@ export const RightCostSavingsCover = styled.div`
   box-shadow: 0px 4px 24px rgba(149, 166, 229, 0.16);
   background: #ffffff;
   border-radius: 12px;
-  padding: 65px 40px 40px;
-`;
-export const RightCostSavingsHeading = styled.h6`
+  padding: 40px 40px 20px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media ${device.mobile} {
+    width: 50%;
+    margin-top: 2rem;
+    margin-left: 0rem;
+  }
+  @media ${device.laptop} {
+    width: 40%;
+    margin-left: 4rem;
+    margin-top: 0rem;
+  }
 `;
-export const RightCostSavingsPrice = styled.p``;
-export const RightCostSavingsPara = styled.p``;
-export const RightCostSavingsCost = styled.p``;
-export const RightCostSavingsFooter = styled.h6``;
+export const RightCostSavingsHeading = styled.h6`
+  font-size: 16px;
+  line-height: 16px;
+  color: #000;
+  font-weight: 700;
+`;
+export const RightCostSavingsPrice = styled.p`
+  display: flex;
+  margin: 1.2rem 0;
+  font-weight: 800;
+`;
+export const RightCostSavingsPara = styled.p`
+  font-size: 15px;
+  line-height: 15px;
+  @media ${device.mobileM} {
+    font-size: 12px;
+    margin: 1.2rem 0;
+  }
+`;
+export const RightCostSavingsCost = styled.p`
+  margin-top: 1.5rem;
+  font-size: 15px;
+  line-height: 15px;
+  align-items: center;
+  margin: 2.5rem 0;
+  @media ${device.mobileM} {
+    font-size: 12px;
+    margin: 1.2rem 0;
+  }
+`;
+
+export const RightCostSavingsCostSpan = styled.span`
+  margin-left: 2.5rem;
+  font-weight: bold;
+  @media ${device.mobileM} {
+    margin-left: 1.5rem;
+  }
+`;
+
+export const RightCostSavingsExact = styled.p`
+  margin: 1.5rem;
+  font-size: 14px;
+  line-height: 14px;
+  margin: 2.5rem 0;
+`;
+export const RightCostSavingsButton = styled.button`
+  color: #3c50e0;
+  padding: 16px 10px;
+  border: solid 1px rgba(83, 96, 131, 0.1);
+  background: #fafcff;
+  border-radius: 8px;
+  font-size: 16px;
+  line-height: 16px;
+  text-align: center;
+  letter-spacing: 0.142857px;
+  min-width: 175px;
+  cursor: pointer;
+
+  &:hover {
+    background: #ebf3ff;
+  }
+`;
+
+export const RightCostSavingsFooter = styled.h6`
+  margin: 1.5rem 0;
+`;
 
 export const RightCostSavings = (props) => {
   return (
     <RightCostSavingsCover>
-      <RightCostSavingsHeading>
-        Estimated Annual Savings
-        <RightCostSavingsPrice>$ 12480</RightCostSavingsPrice>
-        <RightCostSavingsPara>
-          <strong>Save&nbsp;60%</strong>
-          on your current spend
-        </RightCostSavingsPara>
-        <RightCostSavingsCost>Yearly Insourcing Cost $20,800</RightCostSavingsCost>
-        <RightCostSavingsCost>Yearly Outsourcing Cost $8,320</RightCostSavingsCost>
-        <Button text="Get in touch" bg="#3C50E0" br="4px" />
-        <RightCostSavingsFooter>Terms &amp; Conditions?</RightCostSavingsFooter>
-      </RightCostSavingsHeading>
+      <RightCostSavingsHeading>Estimated Annual Savings</RightCostSavingsHeading>
+      <RightCostSavingsPrice>
+        <Text color="#3c50e0" text="$" margin="5rem"></Text>
+        <Text color="#3c50e0" text="12,480" size="text2" />
+      </RightCostSavingsPrice>
+      <RightCostSavingsPara>
+        <strong>Save&nbsp;60% </strong>
+        on your current spend
+      </RightCostSavingsPara>
+      <RightCostSavingsCost>
+        Yearly Insourcing Cost <RightCostSavingsCostSpan>$20,800</RightCostSavingsCostSpan>
+      </RightCostSavingsCost>
+      <RightCostSavingsCost>
+        Yearly Outsourcing Cost
+        <RightCostSavingsCostSpan>$8,320</RightCostSavingsCostSpan>
+      </RightCostSavingsCost>
+      <RightCostSavingsExact>Want to know your exact savings?</RightCostSavingsExact>
+      <RightCostSavingsButton>GET IN TOUCH</RightCostSavingsButton>
+      <RightCostSavingsFooter>Terms &amp; Conditions?</RightCostSavingsFooter>
     </RightCostSavingsCover>
   );
 };
--- src/components/organisms/CostSavings.jsx
+++ src/components/organisms/CostSavings.jsx
@@ -3,22 +3,38 @@ import styled from 'styled-components/macro';
 import { connect } from 'react-redux';
 import { LeftCostSavings } from '../molecules';
 import { RightCostSavings } from '../molecules';
+import { device } from '../../theme';
 
-export const CostSavingsWrapper = styled.div`
+export const CostSavingsWrapper = styled.section``;
+
+export const CostSavingsInsideWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   flex: 1;
   background-color: #ff5670;
   padding: 125px 40px;
   border-radius: 0 100px 0 100px;
-  width: 100%;
+  width: 80%;
+  margin: 0 auto;
+  padding: 40px 40px;
+  overflow-x: hidden;
+  @media ${device.mobileM} {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  @media ${device.laptop} {
+    flex-direction: row;
+  }
 `;
 
 export const CostSavings = () => {
   return (
     <CostSavingsWrapper>
-      <LeftCostSavings />
-      <RightCostSavings />
+      <CostSavingsInsideWrapper>
+        <LeftCostSavings />
+        <RightCostSavings />
+      </CostSavingsInsideWrapper>
     </CostSavingsWrapper>
   );
 };
